57c20a53632134c09ad0bb3d833a5112b12b4ec2
danklut <danklutmeyer@hotmail.ca>
2018-08-04 03:51:39 -0700

    Update netlify_redirect.js

--- static/admin/netlify_redirect.js
+++ static/admin/netlify_redirect.js
@@ -1,9 +1,12 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on('init', user => {
-    if (!user) {
-      window.netlifyIdentity.on('login', () => {
-        document.location.href = '/admin/'
-      })
-    }
-  })
-}
+<script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
+
